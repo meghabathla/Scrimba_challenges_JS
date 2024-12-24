fc344382fbe4ba256e43b7fc237fe944eb5f927d
Megha <bathlamegha6@gmail.com>
2024-12-25 01:24:40 +0530

    done

---
+++ shuffle.js
@@ -0,0 +1,22 @@
+function shuffleArray(arr) {
+  // Repeat the array and shuffle it
+  const doubledArray = [...arr, ...arr];
+  console.log(Math.random() - 0.5);
+  // Shuffle using sort and a random comparison function
+  return doubledArray.sort(() => Math.random() - 0.5);
+}
+
+const myArray = [1, 2, 3, 4];
+const shuffledArray = shuffleArray(myArray);
+console.log(shuffledArray);
+// --------------------------------------- Rough :-|
+// function shuffleArray(array) {
+//   let shuffledArray = [];
+//   for (let i = 0; i < array.length; i--) {
+//     const randomIndex = Math.floor(Math.random() * i + 1);
+//     shuffleArray.push(array[randomIndex]);
+//   }
+//   console.log(shuffledArray);
+//   return shuffleArray;
+// }
+// shuffledArray(emojis);
